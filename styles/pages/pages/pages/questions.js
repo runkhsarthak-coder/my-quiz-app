257274af49ebd3bebd08772bffab4c1ab87eed80
runkhsarthak-coder <runkhsarthak@gmail.com>
2025-09-14 18:34:19 +0530

    Create questions.js

---
+++ styles/pages/pages/pages/questions.js
@@ -0,0 +1,6 @@
+const questions = [
+  { question: "What is 2 + 2?", options: ["3", "4", "5", "6"], correct: "4" },
+  { question: "Which is a mammal?", options: ["Shark", "Dolphin", "Frog", "Octopus"], correct: "Dolphin" },
+  { question: "What is the capital of France?", options: ["Paris", "London", "Rome", "Berlin"], correct: "Paris" },
+];
+export default questions;
